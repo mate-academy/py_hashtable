e54ed1e4edd387690d391fe6bd7883fa6f5c0672
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-26 11:35:08 +0200

    1st commit

--- hashtable.py
+++ hashtable.py
@@ -1,14 +1,20 @@
-class HashTable:
-    def __init__(self):
-        pass
+"""docstring"""
 
-    def hash_(self, x: int) -> int:
-        return x%10
+
+class HashTable:
+    """hash tabe implementation"""
+    def __init__(self):
+        self.hash_table = [None] * 20
+
+    @staticmethod
+    def hash_(val: int) -> int:
+        """hash function"""
+        return val%10
 
     def get(self, key: int) -> int:
-
-        return 42
+        """get value from table by key"""
+        return self.hash_table[key]
 
     def set(self, key: int, value: int) -> None:
-
-        pass
+        """add value by key"""
+        self.hash_table[key] = value
