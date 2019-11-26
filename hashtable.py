41813f0410146a1d1637c9bcdc14e6b4d34d9934
Dima <dima.sapsay@gmail.com>
2019-11-26 12:45:44 +0200

    first commit

--- hashtable.py
+++ hashtable.py
@@ -1,14 +1,24 @@
-class HashTable:
-    def __init__(self):
-        pass
+"""
+Implement hash table using suggested hash function
+"""
 
-    def hash_(self, x: int) -> int:
-        return x%10
+
+def hash_(num: int) -> int:
+    """Func"""
+    return num % 10
+
+
+class HashTable:
+    """
+    Class
+    """
+    def __init__(self):
+        self.hash_dict = {}
 
     def get(self, key: int) -> int:
-
-        return 42
+        """Get value"""
+        return self.hash_dict[key]
 
     def set(self, key: int, value: int) -> None:
-
-        pass
+        """Set value"""
+        self.hash_dict[key] = value
