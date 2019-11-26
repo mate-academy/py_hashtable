8aed4341d888f1b501c484b29d96a809821be09e
vlysenok <tina.lysenok@gmail.com>
2019-11-26 15:25:25 +0200

    hw7_py_hashtable

--- hashtable.py
+++ hashtable.py
@@ -1,14 +1,40 @@
+"""
+module hashtable
+"""
+
+
 class HashTable:
+    """
+    Implementation of hash table.
+    """
+
     def __init__(self):
-        pass
+        self.size = 100
+        self.table = [None] * self.size
 
-    def hash_(self, x: int) -> int:
-        return x%10
+    def hash_(self, key: int) -> int:
+        """
 
-    def get(self, key: int) -> int:
+        :param key: int
+        :return: int
+        """
+        return id(key) % len(self.table)
 
-        return 42
+    def get(self, key: int) -> None:
+        """
+        Get object with key from hash table.
+        :param key: int
+        :return: None
+        """
+        hash_key = self.hash_(key)
+        return self.table[hash_key]
 
     def set(self, key: int, value: int) -> None:
-
-        pass
+        """
+        Insert object with key into hash table.
+        :param key: int
+        :param value: int
+        :return: None
+        """
+        hash_key = self.hash_(key)
+        self.table[hash_key] = value
