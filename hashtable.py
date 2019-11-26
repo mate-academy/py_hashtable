4c3590cbf40294e18e3c0de9b05325396ebbddf2
Senku1 <morhaliuk@gmail.com>
2019-11-26 14:24:23 +0200

    Solved

--- hashtable.py
+++ hashtable.py
@@ -1,14 +1,40 @@
+"""doc-string"""
+
+
 class HashTable:
+    """
+    HashTable class
+    """
     def __init__(self):
-        pass
+        self.table = [[] for _ in range(10)]
 
-    def hash_(self, x: int) -> int:
-        return x%10
+    @staticmethod
+    def hash_(key: int) -> int:
+        """
+        Returns coded value of the key
+        :param key: int
+        :return: int
+        """
+        return key % 10
 
-    def get(self, key: int) -> int:
-
-        return 42
+    def get(self, key: int):
+        """
+        Returns value respectively to the given key
+        :param key: int
+        :return: int
+        """
+        idx = self.hash_(key)
+        for keys, val in self.table[idx]:
+            if keys == key:
+                return val
+        return "Not found!!!"
 
     def set(self, key: int, value: int) -> None:
-
-        pass
+        """
+        Adds given key, value to the hashtable
+        :param key: int
+        :param value: int
+        :return: None
+        """
+        idx = self.hash_(key)
+        self.table[idx].append((key, value))
--- test_hashtable.py
+++ test_hashtable.py
@@ -9,7 +9,7 @@ def test_hashtable1():
     assert h.get(1) == 1
 
 
-def test_hashtable1():
+def test_hashtable2():
     h = hashtable.HashTable()
     h.set(1, 1)
     h.set(11, 2)
@@ -17,7 +17,7 @@ def test_hashtable1():
     assert h.get(11) == 2
 
 
-def test_hashtable2():
+def test_hashtable3():
     h = hashtable.HashTable()
     h.set(1, 1)
     h.set(11, 2)
