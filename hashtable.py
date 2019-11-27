e56c2791fcfd90cc7243f0dec57c7c5d30e8a6b4
d1makov <makovskyj.danylo@gmail.com>
2019-11-27 19:52:33 +0200

    final

--- hashtable.py
+++ hashtable.py
@@ -1,14 +1,42 @@
-class HashTable:
-    def __init__(self):
-        pass
+"""
+docstring
+"""
 
-    def hash_(self, x: int) -> int:
-        return x%10
+
+class HashTable:
+    """
+    class docstring
+    """
+    def __init__(self):
+        self.hash_table = [[]] * 10
+
+    def hash_(self, value: int) -> int:
+        """
+
+        :param value:
+        :param x:
+        :return:
+        """
+        return value % len(self.hash_table)
 
     def get(self, key: int) -> int:
+        """
 
-        return 42
+        :param key:
+        :return:
+        """
+        index = self.hash_(key)
+        for keys, val in self.hash_table[index]:
+            if keys == key:
+                return val
+        return False
 
     def set(self, key: int, value: int) -> None:
+        """
 
-        pass
+        :param key:
+        :param value:
+        :return:
+        """
+        hash_key = self.hash_(key)
+        self.hash_table[hash_key].append((key, value))
