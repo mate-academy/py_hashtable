344c673f9e41335fdc88fff5ec6d06a27f8f4267
Dima <dima.sapsay@gmail.com>
2019-11-27 11:12:14 +0200

    second commit

--- hashtable.py
+++ hashtable.py
@@ -3,22 +3,38 @@ Implement hash table using suggested hash function
 """
 
 
-def hash_(num: int) -> int:
-    """Func"""
-    return num % 10
-
-
 class HashTable:
     """
     Class
     """
     def __init__(self):
-        self.hash_dict = {}
+        self.hash_table = [[] for _ in range(10)]
+
+    def hash_(self, num: int) -> int:
+        """Func"""
+        return num % 10
 
     def get(self, key: int) -> int:
         """Get value"""
-        return self.hash_dict[key]
+        hash_key = self.hash_(key)
+        bucket = self.hash_table[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                return v
 
     def set(self, key: int, value: int) -> None:
         """Set value"""
-        self.hash_dict[key] = value
+        print('key: ', key, 'value: ', value)
+        hash_key = self.hash_(key)
+        key_exists = False
+        bucket = self.hash_table[hash_key]
+        for i, kv in enumerate(bucket):
+            k, v = kv
+            if key == k:
+                key_exists = True
+                break
+        if key_exists:
+            bucket[i] = ((key, value))
+        else:
+            bucket.append((key, value))
