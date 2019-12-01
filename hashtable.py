d3899cce3e69065b397df45a5b497236c51ca613
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-01 15:04:27 +0200

    The script is re-written according to remarks obtained. Seems the easier the task is for other students the harder it's for me:)

--- hashtable.py
+++ hashtable.py
@@ -1,19 +1,20 @@
 """module docstring"""
+from typing import List
+
 class HashTable:
     """class docstring"""
-    def __init__(self):
-        """docstring"""
-        self.hashtable = {}
 
-    @staticmethod
-    def hash(init_value):
+    hash_index = 10
+    hashtable = [[] for x in range(hash_index)] # type: List[List[int]]
+
+    def hash(self, key):
         """docstring"""
-        return init_value % 10
+        return key % self.hash_index
 
     def get(self, key):
         """docstring"""
-        return self.hashtable[hash(key)]
+        return [x[1] for x in self.hashtable[self.hash(key)] if x[0] == key][0]
 
     def set(self, key, value):
         """docstring"""
-        self.hashtable[hash(key)] = value
+        self.hashtable[self.hash(key)].append((key, value))
